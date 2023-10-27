0a888fbf2ba69cb169e4950524db9613c8d066d4
MadScientist_Alf <114734068+MadSCI-entist@users.noreply.github.com>
2023-10-27 15:33:45 +0400

    Update SigNO GLove.py
    Added COM PORTS

--- ESapp/Code/SigNO GLove.py
+++ ESapp/Code/SigNO GLove.py
@@ -1,12 +1,20 @@
 from PIL import Image
 from PIL import ImageTk
-
 import tkinter as tk
 from tkinter import ttk
-
 import json
-
 import pyttsx3
+import serial
+import time
+
+
+# Configure the serial connections
+arduino1_port = "COM4"  # Replace with the actual COM port for Arduino 1
+arduino2_port = "COM6"  # Replace with the actual COM port for Arduino 2
+baud_rate = 9600
+
+arduino1_serial = serial.Serial(arduino1_port, baud_rate)
+arduino2_serial = serial.Serial(arduino2_port, baud_rate)
 
 
 
@@ -46,6 +54,7 @@ def TTS():
 	title_label.place_forget()
 	TTS_entry.pack(pady=350)
 	Back_2.place(x=260, y=5)
+	start.pack()
 	Say.pack()
 
  
@@ -100,7 +109,32 @@ def volume_set():
 def lambda_func():
 	volume_position_save()
 	volume_set()
+
+def start():
+	while True:
+		# Read data from Arduino 1
+		data1 = arduino1_serial.readline().decode().strip()
+		# Read data from Arduino 2
+		data2 = arduino2_serial.readline().decode().strip()
+
+		# Process data from Arduino 1
+		sensor_data1 = data1.split(",")
 		
+		# Process data from Arduino 2
+		sensor_data2 = data2.split(",")
+		
+		# Print or process the sensor data from both Arduino
+		print("Arduino 1 Sensor Data:", sensor_data1)
+		print("Arduino 2 Sensor Data:", sensor_data2)
+
+		X1, Y1, Z1 , F1, F2, F3, F4, F5 = sensor_data1
+		X2, Y2, Z2 , F6, F7, F8, F9, F10 = sensor_data2
+
+		time.sleep(0.5)
+		
+
+
+
 
 
 
@@ -120,6 +154,7 @@ title_label.pack(side = 'top', anchor = 'n')
 #input_frame.configure(bg = 'blue')
 Bck = tk.Button(window,text = 'Back', command = Back )
 Back_2 = tk.Button(window,text = 'Back', command = Bck_2 )
+start = tk.Button(window,text = 'Start', command = start )
 Say = tk.Button(window,text = 'Say', command = TTS_convert )
 
 TTS_entry = tk.Entry(window, font =('Arial',20))
