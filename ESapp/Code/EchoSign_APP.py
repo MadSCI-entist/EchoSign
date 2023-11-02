d46761e0e29b5d48e79c6a64f4b4fcfa581277f2
PhilosopherOfCode <148618956+PhilosopherOfCode@users.noreply.github.com>
2023-11-02 16:58:15 +0400

    Update EchoSign_APP.py
    Try and pass added

--- ESapp/Code/EchoSign_APP.py
+++ ESapp/Code/EchoSign_APP.py
@@ -8,8 +8,6 @@ import serial
 import time
 import keyboard
 
-import serial
-import time
 # Configure the serial connections
 arduino1_port = "COM4"  # Replace with the actual COM port for Arduino 1
 arduino2_port = "COM6"  # Replace with the actual COM port for Arduino 2
@@ -18,29 +16,6 @@ baud_rate = 9600
 arduino1_serial = serial.Serial(arduino1_port, baud_rate)
 arduino2_serial = serial.Serial(arduino2_port, baud_rate)
 
-while True:
-    # Read data from Arduino 1
-    data1 = arduino1_serial.readline().decode().strip()
-    # Read data from Arduino 2
-    data2 = arduino2_serial.readline().decode().strip()
-
-    # Process data from Arduino 1
-    sensor_data1 = data1.split(",")
-    
-    # Process data from Arduino 2
-    sensor_data2 = data2.split(",")
-    
-    # Print or process the sensor data from both Arduino
-    print("Arduino 1 Sensor Data:", sensor_data1)
-    print("Arduino 2 Sensor Data:", sensor_data2)
-
-    X1, Y1, Z1 , F1, F2, F3, F4, F5 = sensor_data1
-    X2, Y2, Z2 , F6, F7, F8, F9, F10 = sensor_data2
-
-    time.sleep(0.5)
-	
-    if keyboard.is_pressed("]"):
-        break
 
 
 
@@ -130,24 +105,40 @@ def volume_set():
 		
 		Engine.setProperty('volume',decibals/10)
 
+def sign_convert():
+	while True:
+    # Read data from Arduino 1
+    	data1 = arduino1_serial.readline().decode().strip()
+    # Read data from Arduino 2
+    	data2 = arduino2_serial.readline().decode().strip()
+
+    # Process data from Arduino 1
+    	sensor_data1 = data1.split(",")
+    
+    # Process data from Arduino 2
+    	sensor_data2 = data2.split(",")
+    
+    # Print or process the sensor data from both Arduino
+    	try:
+    		print("Arduino 1 Sensor Data:", sensor_data1)
+    		print("Arduino 2 Sensor Data:", sensor_data2)
+    	except:
+    		print('Recieved less than 8 values')
+
+    	X1, Y1, Z1 , F1, F2, F3, F4, F5 = sensor_data1
+    	X2, Y2, Z2 , F6, F7, F8, F9, F10 = sensor_data2
+
+    	time.sleep(0.5)
+	
+    	if keyboard.is_pressed("}"):
+    		Break
+        	
+
+
+
 def lambda_func():
 	volume_position_save()
 	volume_set()
-
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
 
 # Header
 title_label = tk.Label(master = window, text = 'SignoGlove', font = ('SegoeUI 24 bold italic',20,'italic','bold'))
@@ -182,5 +173,4 @@ Engine = pyttsx3.init()
 
 #run
 window.mainloop()
-
 
